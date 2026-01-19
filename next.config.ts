05df46a5b2e5289a0baba0be676f52bc0a9c74ef
Joshua Juarez <hj.juarezmejia@gmail.com>
2026-01-19 17:51:18 -0600

    primer commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com'
+      }
+
+    ]
+  }
 };
 
 export default nextConfig;
